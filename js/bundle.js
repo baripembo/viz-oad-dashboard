fbee5ee82d6e6ef5e44bfac3ef41e35b49bd1e3d
baripembo <erikawei@gmail.com>
2020-08-12 09:25:01 -0400

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -380,9 +380,9 @@ function createRankingChart() {
   }
 
   //switch sort dropdown if on covid layer
-  if (currentIndicator.id=='#covid+weekly+cases+per+capita') {
+  if (currentIndicator.id=='#covid+cases+per+capita') {
     $('.ranking-container').addClass('covid');
-    $('.ranking-select').val('#covid+weekly+cases+per+capita');
+    $('.ranking-select').val('#covid+cases+per+capita');
   }
   else {
     $('.ranking-container').removeClass('covid');
@@ -1480,7 +1480,7 @@ function setGlobalFigures() {
 		createKeyFigure('.figures', 'Number of Countries', '', totalCountries);
 	}
 	//covid figures
-	else if (currentIndicator.id=='#covid+weekly+cases+per+capita') {
+	else if (currentIndicator.id=='#covid+cases+per+capita') {
 		var totalCases = d3.sum(nationalData, function(d) { 
 			if (regionMatch(d['#region+name']))
 				return d['#affected+infected']; 
@@ -2054,7 +2054,7 @@ function getGlobalLegendScale() {
 
   //set scale
   var scale;
-  if (currentIndicator.id=='#covid+weekly+cases+per+capita') {
+  if (currentIndicator.id=='#covid+cases+per+capita') {
     var data = [];
     nationalData.forEach(function(d) {
       if (d[currentIndicator.id]!=null && regionMatch(d['#region+name']))
@@ -2200,7 +2200,7 @@ function setGlobalLegend(scale) {
     else {
       $('.legend-container').removeClass('access-severity');
       var legendFormat = (currentIndicator.id.indexOf('pct')>-1 || currentIndicator.id.indexOf('ratio')>-1) ? d3.format('.0%') : shortenNumFormat;
-      if (currentIndicator.id=='#covid+weekly+cases+per+capita') legendFormat = d3.format('.1f');
+      if (currentIndicator.id=='#covid+cases+per+capita') legendFormat = d3.format('.1f');
       legend = d3.legendColor()
         .labelFormat(legendFormat)
         .cells(colorRange.length)
@@ -2496,7 +2496,7 @@ function createMapTooltip(country_code, country_name) {
     var content = '<h2>' + country_name + '</h2>';
 
     //COVID trend layer shows sparklines
-    if (currentIndicator.id=='#covid+weekly+cases+per+capita') {
+    if (currentIndicator.id=='#covid+cases+per+capita') {
       content += "Weekly Number of New Cases" + ':<div class="stat covid-cases">' + numFormat(country[0]['#covid+weekly+cases']) + '</div>';
       content += "Weekly Number of New Deaths" + ':<div class="stat covid-deaths">' + numFormat(country[0]['#covid+weekly+deaths']) + '</div>';
       content += "Weekly Trend (new cases past week / prior week)" + ':<div class="stat covid-pct">' + percentFormat(country[0]['#covid+trend+pct']) + '</div>';
@@ -2621,7 +2621,7 @@ function createMapTooltip(country_code, country_name) {
     tooltip.setHTML(content);
 
     //COVID cases layer charts -- inject this after divs are created in tooltip
-    if (currentIndicator.id=='#covid+weekly+cases+per+capita' && val!='No Data') {
+    if (currentIndicator.id=='#covid+cases+per+capita' && val!='No Data') {
       //weekly cases sparkline
       var sparklineArray = [];
       covidTrendData[country_code].forEach(function(d) {
@@ -2880,7 +2880,7 @@ $( document ).ready(function() {
         //store covid trend data
         var covidByCountry = covidTrendData[item['#country+code']];
         item['#covid+trend+pct'] = (covidByCountry==undefined) ? null : covidByCountry[covidByCountry.length-1].weekly_new_cases_pc_change/100;
-        item['#covid+weekly+cases+per+capita'] = (covidByCountry==undefined) ? null : covidByCountry[covidByCountry.length-1].weekly_new_cases_per_ht;
+        item['#covid+cases+per+capita'] = (covidByCountry==undefined) ? null : covidByCountry[covidByCountry.length-1].weekly_new_cases_per_ht;
         item['#covid+weekly+cases'] = (covidByCountry==undefined) ? null : covidByCountry[covidByCountry.length-1].weekly_new_cases;
         item['#covid+weekly+deaths'] = (covidByCountry==undefined) ? null : covidByCountry[covidByCountry.length-1].weekly_new_deaths;
         item['#covid+total+cases+per+capita'] = (item['#affected+infected'] / item['#population']) * 100000;
