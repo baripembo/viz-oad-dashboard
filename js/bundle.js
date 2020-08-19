85d608ac22644c3c67aec59e8f1bc984b11ae70e
baripembo <erikawei@gmail.com>
2020-08-19 15:18:56 -0400

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -1813,8 +1813,7 @@ function createEvents() {
   //back to global event
   $('.country-panel h2').on('click', function() {
     resetMap();
-    console.log(window.location)
-    //window.history.replaceState(null, null, '/');
+    window.history.replaceState(null, null, window.location.pathname);
   });
 
   //country panel indicator select event
