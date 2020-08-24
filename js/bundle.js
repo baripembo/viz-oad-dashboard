7f0570a830dcc1b5a55b2a7c5a78291b1d76bb15
baripembo <erikawei@gmail.com>
2020-08-24 16:43:54 -0400

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -2188,10 +2188,9 @@ function setGlobalLegend(scale) {
     markersvg.select('.legendSize')
       .call(legendSize);
 
-
     //gender disaggregation explanatory text
     var genderDataText = '*Gender breakdown explanation goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
-    $('.map-legend.global').append('<h4>(On hover) COVID-19 Sex-Disaggregated Data Tracker</h4>');
+    $('.map-legend.global').append('<h4>COVID-19 Sex-Disaggregated Data Tracker</h4>');
     createSource($('.map-legend.global'), '#affected+killed+m+pct');
     $('.map-legend.global').append('<p class="footnote gender-data small">'+ truncateString(genderDataText, 65) +' <a href="#" class="expand">MORE</a></p>');
     $('.map-legend.global .gender-data').click(function() {
