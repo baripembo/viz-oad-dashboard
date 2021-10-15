7421472f95e873e91f31e5b07a4e73db4520b679
baripembo <erikawei@gmail.com>
2021-10-15 14:29:41 -0400

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -1859,6 +1859,9 @@ function setKeyFigures() {
 	secondaryPanel.find('.figures, .source-container, .ranking-chart').empty();
 	secondaryPanel.find('.source-container').show();
 
+	//title
+	secondaryPanel.find('.secondary-panel-title').html(currentIndicator.title);
+
 	//source
 	var indicator = currentIndicator.id;
 	if (indicator=='#affected+inneed+pct') indicator = '#affected+inneed';
@@ -2177,7 +2180,7 @@ function displayMap() {
   }
 
   //set initial indicator
-  currentIndicator = {id: $('.menu-indicators').find('.selected').attr('data-id'), name: $('.menu-indicators').find('.selected').attr('data-legend')};
+  currentIndicator = {id: $('.menu-indicators').find('.selected').attr('data-id'), name: $('.menu-indicators').find('.selected').attr('data-legend'), title: $('.menu-indicators').find('.selected').text()};
 
   //init element events
   createEvents();
@@ -2447,7 +2450,7 @@ function selectLayer(menuItem) {
     toggleSecondaryPanel(menuItem);
   }
   else {
-    currentIndicator = {id: $(menuItem).attr('data-id'), name: $(menuItem).attr('data-legend')};
+    currentIndicator = {id: $(menuItem).attr('data-id'), name: $(menuItem).attr('data-legend'), title: $(menuItem).text()};
     toggleSecondaryPanel(menuItem, 'open');
 
     //set food prices view
